03314c7441a358e16372d36c09550eb288f775eb
leonardo <lionardinux@hotmail.com>
2022-06-13 13:29:37 +0100

    Added review creation or editing page

--- reviews/views.py
+++ reviews/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
-from .models import Book, Contributor, Publisher
+from django.utils import timezone
+from .models import Book, Contributor, Publisher, Review
 
 from .utils import average_rating
-from .forms import PublisherForm, SearchForm
+from .forms import PublisherForm, SearchForm, ReviewForm
 
 def index(request):
     return render(request, "base.html")
@@ -97,3 +98,37 @@ def publisher_edit(request, pk=None):
 
     return render(request, "reviews/instance-form.html",
                   {"form": form, "instance": publisher, "model_type": "Publisher"})
+
+def review_edit(request, book_pk, review_pk=None):
+    book = get_object_or_404(Book, pk=book_pk)
+
+    if review_pk is not None:
+        review = get_object_or_404(Review, book_id=book_pk, pk=review_pk)
+    else:
+        review = None
+
+    if request.method == "POST":
+        form = ReviewForm(request.POST, instance=review)
+
+        if form.is_valid():
+            updated_review = form.save(False)
+            updated_review.book = book
+
+            if review is None:
+                messages.success(request, "Review for \"{}\" created.".format(book))
+            else:
+                updated_review.date_edited = timezone.now()
+                messages.success(request, "Review for \"{}\" updated.".format(book))
+
+            updated_review.save()
+            return redirect("book_detail", book.pk)
+    else:
+        form = ReviewForm(instance=review)
+
+    return render(request, "reviews/instance-form.html",
+                  {"form": form,
+                   "instance": review,
+                   "model_type": "Review",
+                   "related_instance": book,
+                   "related_model_type": "Book"
+                   })
