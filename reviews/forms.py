fd2264bd5123a49d4444eb0ed795aa8b20d2fd51
leonardo <lionardinux@hotmail.com>
2022-09-22 12:29:14 +0100

    Installed crispy-forms to simplify the rendering of all forms.

--- reviews/forms.py
+++ reviews/forms.py
@@ -3,6 +3,16 @@ from .models import Publisher, Review, Book
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit
 
+class InstanceForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        if kwargs.get("instance"):
+            button_title = "Save"
+        else:
+            button_title = "Create"
+        self.helper.add_input(Submit("", button_title))
+
 
 class SearchForm(forms.Form):
     search = forms.CharField(required=False, min_length=3)
@@ -11,25 +21,25 @@ class SearchForm(forms.Form):
                                       ("title", "Title"),
                                       ("contributor", "Contributor")
                                   ))
-    def __int__(self, *args, **kwargs):
-        super().__int__(*args, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_method = "get"
         self.helper.add_input(Submit("", "Search"))
 
-class PublisherForm(forms.ModelForm):
+class PublisherForm(InstanceForm):
     class Meta:
         model = Publisher
         fields = "__all__"
 
-class ReviewForm(forms.ModelForm):
+class ReviewForm(InstanceForm):
     class Meta:
         model = Review
         exclude = ("book", "date_edited")
 
     rating = forms.IntegerField(min_value=0, max_value=5)
 
-class BookMediaForm(forms.ModelForm):
+class BookMediaForm(InstanceForm):
     class Meta:
         model = Book
         fields = ["cover", "sample"]
